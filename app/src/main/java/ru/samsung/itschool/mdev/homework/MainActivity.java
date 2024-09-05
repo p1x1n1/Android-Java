20f419185df08d1d77071660f2e6e983f7476d4a
p1x1n1 <abazina858@gmail.com>
2024-09-05 21:10:47 +0400

    Добавлено приложение для работы с базой данных SQLite
    1. Реализованы функции для добавления, чтения, обновления и удаления записей в базе данных:
       - Добавление новой записи (имя и email)
       - Чтение всех записей и вывод их в лог
       - Очистка всей таблицы
       - Обновление записи по ID
       - Удаление записи по ID
    
    2. Добавлен интерфейс с кнопками для выполнения вышеуказанных операций и полями для ввода данных:
       - Кнопка "Добавить" для добавления новой записи
       - Кнопка "Читать" для вывода всех записей в лог
       - Кнопка "Очистить" для удаления всех записей
       - Кнопка "Обновить" для изменения записи по ID
       - Кнопка "Удалить" для удаления записи по ID
    
    3. Создана таблица в базе данных с тремя столбцами: id (автоинкремент), name (текст), email (текст).
    
    4. Настроен вид GridView для отображения данных в приложении.

---
+++ app/src/main/java/ru/samsung/itschool/mdev/homework/MainActivity.java
@@ -0,0 +1,182 @@
+package ru.samsung.itschool.mdev.homework;
+
+import android.app.Activity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.GridView;
+
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    String[] data = {"a","b","c","d","e","f","g","h","i","j","k"};
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
+
+    Button btnAdd,btnRead,btnClear, btnUpd, btnDel;
+
+    EditText etName, etEmail,etID;
+
+    DBHelper dbHelper;
+
+    /** Called when the activity is first created */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<String>(this,R.layout.item);
+        gvMain = (GridView) findViewById(R.id.gvMain);
+        gvMain.setAdapter(adapter);
+        adjustGridView();
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+        btnUpd = (Button) findViewById(R.id.btnUpd);
+        btnUpd.setOnClickListener(this);
+
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnDel.setOnClickListener(this);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etID = (EditText) findViewById(R.id.etID);
+
+        //object for create and mange db
+
+        dbHelper = new DBHelper(this);
+    }
+    private void adjustGridView(){
+        gvMain.setNumColumns(GridView.AUTO_FIT);
+        gvMain.setColumnWidth(50);
+        gvMain.setVerticalSpacing(5);
+        gvMain.setHorizontalSpacing(5);
+        gvMain.setStretchMode(GridView.NO_STRETCH);
+    }
+    @Override
+    public void onClick(View v){
+        //object for date
+        ContentValues cv = new ContentValues();
+
+        //get date from input
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+        String id = etID.getText().toString();
+
+        //connect to db
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        switch ( ( v.getId())){
+            case R.id.btnAdd:
+                Log.d(LOG_TAG,"---Insert in mytable: ---");
+                //подготовим данные для вставки пар: наименование столбца - значение
+
+                cv.put("name",name);
+                cv.put("email",email);
+                //вставляем запись и получаем ее ID
+                long rowID  = db.insert("mytable",null,cv);
+
+                Log.d(LOG_TAG,"row iserted, ID = " + rowID);
+                break;
+            case R.id.btnRead:
+                Log.d(LOG_TAG,"---Rows in mytable: ---");
+                //select date
+                Cursor c = db.query("mytable",null,null,null,null,null,null);
+
+                //ставим позицию курсора на первую строку выборки
+                //если в выборке нет строк, вернется false
+                if (c.moveToFirst()){
+
+                    //определеяем номера столбцов по имени в выборке
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+
+                    do{
+                        //получаем значения по номерам столбцов и пишем все в лог
+                        Log.d(LOG_TAG,
+                                "ID = "+c.getInt(idColIndex) +
+                                        ", name = "+ c.getString(nameColIndex)+
+                                        ", email = " + c.getString(emailColIndex)
+                                );
+                        //переход наслед строку
+                        //если нет выход из цикла
+
+                    }while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG,"0 rows");
+                c.close();
+                break;
+            case R.id.btnClear:
+                Log.d(LOG_TAG,"---Clear mytable: ---");
+                //удаляем все данные
+                int clearCount = db.delete("mytable",null,null);
+                Log.d(LOG_TAG,"deleted rows count = " + clearCount);
+                break;
+            case R.id.btnUpd:
+                if (id.equalsIgnoreCase("")){
+                    break;
+                }
+                Log.d(LOG_TAG,"---Update mytable: ---");
+                //подготовим значения для обновления
+                cv.put("name",name);
+                cv.put("email",email);
+                //обновим по id
+                int updCount = db.update("mytable",cv, " id = ? ",
+                        new String[] {id});
+                Log.d(LOG_TAG, "updated rows count = " + updCount);
+                break;
+            case R.id.btnDel:
+                if (id.equalsIgnoreCase("")){
+                    break;
+                }
+                Log.d(LOG_TAG,"---Delete from mytable: ---");
+                //удаляем по id
+                int delCount = db.delete("mytable", "id = " + id,
+                        null);
+                Log.d(LOG_TAG,"deleted rows count = " + delCount);
+                break;
+        }
+        dbHelper.close();
+    }
+    class DBHelper extends SQLiteOpenHelper{
+
+        public DBHelper(Context context){
+            super(context,"myDB", null,1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            Log.d(LOG_TAG,"---onCreate database ---");
+            db.execSQL("create table mytable (" +
+                    "id integer primary key autoincrement," +
+                    "name text,"+
+                    "email text"+
+                    ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+        }
+    }
+}
